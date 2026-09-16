10c419622d317e6540f575eb91aaceb6e4f5fe76
samirsgs <samirmagid12@gmail.com>
2026-09-16 14:49:48 +0200

    Merge changes

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ cookie.addEventListener("click", function() {
   void cookie.offsetWidth; // forces a reflow so the animation can replay
   cookie.classList.add("clicked");
 });
+// Creating a class
+class student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    displayInfo() {
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+    }
+}
+
+const samir = new student("samir",20);
+samir.displayInfo();
